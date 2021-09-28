8109aaefd3d09168969d6040fbbeab5bb5f817a5
Bilal Ahmed <bilalsaeedalam1@gmail.com>
2021-09-28 12:24:09 +0500

    Bootstrap css and js files added

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+var expressLayouts = require("express-ejs-layouts");
 const app = express();
 const index = require("./routes/index");
+const users = require("./routes/users");
 
+// EJS
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
+// Routes
 app.use("/", index);
+app.use("/users", users);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
